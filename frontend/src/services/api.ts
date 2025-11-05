2521ac9efaebfabf1fd3b4772129eb278f0075ec
Devendra Ahire <devendrarahire01@gmail.com>
2025-11-05 11:00:51 +0530

    Update api.ts

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'https://prepvista-backend7.onrender.com/api';
+const API_BASE_URL = 'https://huggingface.co/spaces/prepvista975/PrepVista-backend';
 
 const getToken = (): string | null => localStorage.getItem('token');
 
